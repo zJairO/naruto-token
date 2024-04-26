0d2c838ad9c411883d29c0cd300b2659bf9db5f3
zJairO <hello@zjairo.com>
2024-04-26 13:56:16 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import logo from './logo.jpeg';
+import about from './about-w.jpeg';
 import './App.css';
+import { Container, Row, Col, Navbar, Nav, Image, h1, h2, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="justify-content-center mt-2">
+        <Col>
+          <Navbar expand="lg" className="bg-black">
+            <Container>
+              <Navbar.Brand href="#"><Image src={logo} width={100} roundedCircle/></Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto menu">
+                  <Nav.Link href="#">Home</Nav.Link>
+                  <Nav.Link href="#">About Us</Nav.Link>
+                  <Nav.Link href="#">Tokenomics</Nav.Link>
+                  <Nav.Link href="#">Buy</Nav.Link>
+                  <Nav.Link href="#">Community</Nav.Link>
+                  <Nav.Link href="#">Contact Us</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </Col>
+      </Row>
+      <Row className='mt-5' style={{
+        backgroundImage: `url(${about})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover'
+      }}>
+        <Col>
+          <h1 className='text-white mt-5'>Welcome to $NARUTO</h1>
+          <h2 className='text-white mt-5'>Join Naruto on his journey to become the Hokage!</h2>
+          <Button className='my-5 btn btn-lg' variant='outline-light'>Buy $NARUTO</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
